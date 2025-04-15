bd50de8c4a77c024033ff0b6409a55c661557d38
Jacek Kujawski <jkujawski1@gmail.com>
2025-04-15 17:42:35 +0200

    feat: Centrelize vertical half moves

--- nvim/lua/config/custom.lua
+++ nvim/lua/config/custom.lua
@@ -5,5 +5,7 @@ vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.g.mapleader = " "
 vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
+vim.keymap.set('n', '<C-d>', '<C-d>zz')
+vim.keymap.set('n', '<C-u>', '<C-u>zz')
 vim.opt.relativenumber = true
 vim.opt.number = true
